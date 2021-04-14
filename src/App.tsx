9966d1b6f57204f34e1327b44ba6cc460901659c
Nemo - Mr Magic <nemo@nemostudios.dev>
2021-04-14 15:10:58 +0200

    Components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {iPokemon, Pokemon} from "./Components/Pokemon";
+import pokeDex from "./Pokedex_logo.png"
+
+
 
 function App() {
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=100')
+        .then(response => response.json())
+        .then(response => {
+         setPokemon(response.results);
+        })
+  }, []);
+
+
+  const [pokemon, setPokemon] = useState<iPokemon[]>([])
+
+
+  const moreInfo = (pokemonPost: iPokemon) => {
+
+
+
+        const newPokemon = pokemon.map(pokemonMatch => {
+          if(pokemonMatch.name === pokemonPost.name){
+            pokemonMatch.showMoreInfo=!pokemonMatch.showMoreInfo;
+            return pokemonMatch;
+          }
+          return pokemonMatch;
+        })
+    setPokemon(newPokemon);
+  }
+
+  const removePokemon = (pokemonPost: iPokemon, key: number) => {
+
+      const newPokemon = [...pokemon]
+          newPokemon.splice(key, 1)
+      setPokemon(newPokemon)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={"app"}>
+      <div className={"header"}>
+      <img src={pokeDex} alt={"Pokedex"}/>
+      </div>
+      <h2 className={"grid"}>
+        {pokemon.map(( pokemonPost, index) => {
+            return <ul> <Pokemon pokemonPost={post => moreInfo(post)} pokemonRemove ={(post: iPokemon, key) => removePokemon(post, key)} pokemon={pokemonPost} key={index}  pokeKey={index}/> </ul>
+        })}
+      </h2>
+      </div>
+    </>
   );
 }
 
